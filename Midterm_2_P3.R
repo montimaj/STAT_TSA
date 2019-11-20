713eb0fcefeb03aaad7e8b058d6d203a2bc7c51c
montimaj <smxnv@mst.edu>
2019-11-20 13:37:05 -0600

    Fixed issues in Midterm P3

--- Midterm_2_P3.R
+++ Midterm_2_P3.R
@@ -13,7 +13,6 @@ library(fUnitRoots)
 data = read.table('Data/ExamData/petr.txt', header=T)
 chemicals.data = ts(data$Chemicals, frequency=12, start=c(1971, 1), end=c(1991, 12))
 vehicles.data = ts(data$Vehicles, frequency=12, start=c(1971, 1), end=c(1991, 12))
-par(mfrow=c(1, 1))
 plot(chemicals.data, main='Time Series Plot of Chemicals Data')
 plot(vehicles.data, main='Time Series Plot of Vehicles Data')
 chemicals.decompose = decompose(chemicals.data, type="mult")  
@@ -25,7 +24,7 @@ plot(vehicles.decompose)
 adf.test(chemicals.data)
 adf.test(vehicles.data)
 
-par(mfrow=c(2, 2))
+par(mfrow=c(2, 1))
 # Differencing to remove linear trend
 chemicals.diff= diff(chemicals.data)
 plot(chemicals.diff, main='Differencing=1: Chemicals Data')
@@ -44,8 +43,9 @@ plot(vehicles.diff, main='Differencing=1: Vehicles Data')
 vehicles.diff2 = diff(vehicles.diff, lag=12)
 plot(vehicles.diff2, main='Lag=12: Vehicles Data')
 # Stationarity Check
-adf.test(chemicals.diff2)
+adf.test(vehicles.diff2)
 
+par(mfrow=c(2, 1))
 # Linear Trend and Harmonic (Seasonal) Trend for Chemicals Data
 har = harmonic(chemicals.data, 1)
 chemicals.fit = lm(chemicals.data ~ har + time(chemicals.data))
@@ -55,7 +55,7 @@ chemicals.model = ts(chemicals.fit$fitted.values, frequency=12, start=c(1971, 1)
 
 plot(chemicals.data, main='TS Plot: Chemicals Data')
 lines(chemicals.model, col='red')
-legend(1975, 25, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.7)
+legend(1975, 25, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.5)
 
 # Checking Transformation for Chemicals Data
 chemicals.bc.lambda = BoxCox.lambda(chemicals.data)
@@ -68,7 +68,7 @@ chemicals.bc.model = ts(chemicals.bc.fit$fitted.values, frequency=12, start=c(19
 
 plot(chemicals.bc.tr, main='TS Plot: Transformed Chemicals Data')
 lines(chemicals.bc.model, col='red')
-legend(1975, 4, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.7)
+legend(1975, 4.2, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.5)
 
 # Linear Trend and Harmonic (Seasonal) Trend for Vehicles Data
 har = harmonic(vehicles.data, 1)
@@ -79,7 +79,7 @@ vehicles.model = ts(vehicles.fit$fitted.values, frequency=12, start=c(1971, 1), 
 
 plot(vehicles.data, main='TS Plot: Vehicles Data')
 lines(vehicles.model, col='red')
-legend(1975, 25, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.7)
+legend(1975, 20, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.5)
 
 # Checking Transformation for Vehicles Data
 vehicles.bc.lambda = BoxCox.lambda(vehicles.data)
@@ -92,9 +92,9 @@ vehicles.bc.model = ts(vehicles.bc.fit$fitted.values, frequency=12, start=c(1971
 
 plot(vehicles.bc.tr, main='TS Plot: Transformed Vehicles Data')
 lines(vehicles.bc.model, col='red')
-legend(1975, 4, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.7)
+legend(1975, 3, legend=c("Actual", "Fitted"), col=c("Black", "Red"), lty=1:1, cex=0.5)
 
-
+par(mfrow=c(2, 2))
 # Differencing after BoxCox Chemicals Data
 chemicals.bc.diff= diff(chemicals.bc.tr)
 plot(chemicals.bc.diff, main='Differencing=1: Transformed Chemicals Data')
@@ -116,45 +116,57 @@ plot(vehicles.bc.diff2, main='Lag=12: Transformed Vehicles Data')
 adf.test(vehicles.bc.diff2)
 
 par(mfrow=c(2, 3))
-# ACF, PACF, and EACF for Chemicals Data
+# ACF, PACF, and EACF for Detrended Chemicals Data
+acf(chemicals.diff2, main="ACF: Detrended Chemicals Data")
+pacf(chemicals.diff2, main="PACF: Detrended Chemicals Data")
+eacf(chemicals.diff2)
+plot(armasubsets(chemicals.diff2, 5, 5))
+
+# ACF, PACF, and EACF for Detrended Vehicles Data
+acf(vehicles.diff2, main="ACF: Detrended Vehicles Data")
+pacf(vehicles.diff2, main="PACF: Detrended Chemicals Data")
+eacf(vehicles.diff2)
+plot(armasubsets(vehicles.diff2, 5, 5))
+
+# ACF, PACF, and EACF for Stabilized Chemicals Data
 acf(chemicals.bc.diff2, main="ACF: Stabilized Chemicals Data")
 pacf(chemicals.bc.diff2, main="PACF: Stabilized Chemicals Data")
 eacf(chemicals.bc.diff2)
 plot(armasubsets(chemicals.bc.diff2, 5, 5))
 
-# ACF, PACF, and EACF for Vehicles Data
+# ACF, PACF, and EACF for Stabilized Vehicles Data
 acf(vehicles.bc.diff2, main="ACF: Stabilized Vehicles Data")
-pacf(vehicles.bc.diff2, main="PACF: Stabilized Chemical Data")
+pacf(vehicles.bc.diff2, main="PACF: Stabilized Chemicals Data")
 eacf(vehicles.bc.diff2)
 plot(armasubsets(vehicles.bc.diff2, 5, 5))
 
 par(mfrow=c(2, 1))
 # ARIMA Model Fits for Chemicals Data
-chemicals.arima = auto.arima(chemicals.data)
-chemicals.bc.arima = auto.arima(chemicals.data, lambda='auto')
+chemicals.arima = auto.arima(chemicals.diff2)
+chemicals.bc.arima = auto.arima(chemicals.diff2, lambda='auto')
 chemicals.bc.diff2.arima = auto.arima(chemicals.bc.diff2)
 
 # ARIMA Model Fits for Vehicles Data
-vehicles.arima = auto.arima(vehicles.data)
-vehicles.bc.arima = auto.arima(vehicles.data, lambda='auto')
+vehicles.arima = auto.arima(vehicles.diff2)
+vehicles.bc.arima = auto.arima(vehicles.diff2, lambda='auto')
 vehicles.bc.diff2.arima = auto.arima(vehicles.bc.diff2)
 
 # Residual Diagnostics (manually repeated for all the models)
 par(mfrow=c(3, 2))
-dataset = chemicals.data
-model = chemicals.model
-st_residuals = chemicals.residuals
-# st_residuals = rstandard(model)
+dataset = vehicles.diff2
+model = chemicals.bc.diff2.arima
+# st_residuals = chemicals.residuals
+st_residuals = rstandard(model)
 plot(y=st_residuals, x=as.vector(time(dataset)), type = 'o', ylab = 'Standardized Residuals', xlab = 'Time', main='Standardized Residual Plot')
 hist(st_residuals, main="Residuals Histogram", xlab='Residuals')
 qqnorm(st_residuals, main="QQ Plot for Residuals") 
 qqline(st_residuals, col="Red")
 boxplot(st_residuals, main='Residual Boxplot', ylab='Residuals')
 acf(st_residuals, main='ACF Plot')
+tsdiag(model)
 # Residual Normality
 shapiro.test(st_residuals)
 snpar::runs.test(st_residuals, exact=TRUE)
-
 # AIC and BIC tests
 AIC(model)
 BIC(model)
